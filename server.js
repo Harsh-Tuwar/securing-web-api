7bcddbbbf88575e8291c642b55bfc0041d7b02ac
Harsh Tuwar <hercules@Harshs-MacBook-Pro.local>
2019-04-15 17:18:20 -0400

    final commit

--- server.js
+++ server.js
@@ -3,21 +3,64 @@ const cors = require("cors");
 const dataService = require("./data-service.js");
 const userService = require("./user-service.js");
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');//middleware for node js
+const passportJWT = require('passport-jwt');
 
 const app = express();
+
+//config the strategy, JSON web token setup
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+//config options
+var jwtOptions = {};
+
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+
+//defining the secret key...64 char long and unguessable string
+jwtOptions.secretOrKey = 'jYy0e1c$9NXcy!CrU62fCfZua%g^!n7a8AZx*JgoMGuNgdvbP^&gMj^BEH$bk6MR';
+
+
+//define strategy
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+
+    if(jwt_payload){
+        next(null, {
+            _id: jwt_payload._id,
+            userName: jwt_payload.userName,
+            fullName: jwt_payload.fullName,
+            role: jwt_payload.role 
+        });
+    } else {
+        next(null, false);
+    }
+});
+
+//telling passport to use our strategy
+passport.use(strategy);
+
+//initialize passport using our famouse app.use();
+app.use(passport.initialize());
 
 app.use(bodyParser.json());
 app.use(cors());
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.get("/api/vehicles",(req,res)=>{
+const token = jwt.sign({ userName: 'bob' }, 'secret', { expiresIn: 60 * 60 });
+
+//use authenticate method to secure our route
+app.get("/api/vehicles", passport.authenticate('jwt', { session: false}), (req,res)=>{
     dataService.getAllVehicles().then((data)=>{
         res.json(data);
     }).catch(()=>{
         res.status(500).end();
     });
 });
+
+
 
 app.post("/api/register", (req, res) => {
     userService.registerUser(req.body)
@@ -33,7 +76,17 @@ app.post("/api/register", (req, res) => {
 app.post("/api/login", (req, res) => {
     userService.checkUser(req.body)
         .then((user) => {
-            res.json({ "message": "login successful" });
+
+            var payload = {
+                _id: user._id,
+                userName: user.userName,
+                fullName: user.fullName,
+                role: user.role
+            };
+
+            var token = jwt.sign(payload, jwtOptions.secretOrKey);
+
+            res.json({ "message": "login successful" , "token": token });
         }).catch((msg) => {
             res.status(422).json({ "message": msg });
         });
